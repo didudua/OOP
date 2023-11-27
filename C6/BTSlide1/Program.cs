5e609da7df9a1b106f384eba99c46674f2fd30aa
didudua <126039750+didudua@users.noreply.github.com>
2023-11-27 15:29:52 +0700

    sapxong

---
+++ C6/BTSlide1/Program.cs
@@ -0,0 +1,121 @@
+﻿public abstract class Hinhhoc
+{
+    public abstract void Chuvi();
+    public abstract void Dientich();
+} 
+public class Hinhchunhat: Hinhhoc
+{
+    private double dai, rong;
+    public void Nhap()
+    {
+        Console.WriteLine("---Hinh Chu Nhat--- ");
+        Console.Write("Nhap chieu dai: ");
+        dai = double.Parse(Console.ReadLine());
+        Console.Write("Nhap chieu rong: ");
+        rong = double.Parse(Console.ReadLine());
+    }
+    public override void Chuvi()
+    {
+        Console.WriteLine($"Chu vi hinh chu nhat = {(dai + rong) * 2}");
+    }
+    public override void Dientich()
+    {
+        Console.WriteLine($"Dien tich hinh chu nhat = {dai*rong}");
+    }
+}
+public class Hinhtron: Hinhhoc
+{
+    private double r, pi = 3.14;
+    public void Nhap()
+    {
+        Console.WriteLine("---Hinh Tron--- ");
+        Console.Write("Nhap ban kinh hinh tron: ");
+        r = double.Parse(Console.ReadLine());
+    }
+    public override void Chuvi()
+    {
+        Console.WriteLine($"Chu vi hinh tron = {2*pi*r}");
+    }
+    public override void Dientich()
+    {
+        Console.WriteLine($"Dien tich hinh tron = {pi*r*r}");
+    }
+}
+public class Hinhtamgiac: Hinhhoc
+{
+    private double a,b,c,P,S;
+    public void Nhap()
+    {
+        Console.WriteLine("---Hinh Tam Giac--- ");
+        Console.Write("Nhap a: ");
+        a = double.Parse(Console.ReadLine());
+        Console.Write("Nhap b: ");
+        b = double.Parse(Console.ReadLine());
+        Console.Write("Nhap c: ");
+        c = double.Parse(Console.ReadLine());
+    }
+    public override void Chuvi()
+    {
+        if (a+b>c & b+c>a & a+c>b)
+        {
+            Console.WriteLine($"Chu vi hinh tam giac = {a+b+c}");
+        }
+        else
+        {
+            Console.WriteLine("Thong tin khong hop le !!!");
+        }
+    }
+    public override void Dientich()
+    {
+        P=(a+b+c)/2;
+        S=Math.Sqrt(P*(P-a)*(P-b)*(P-c)); 
+        if (a+b>c & b+c>a & a+c>b)
+        {  
+            Console.WriteLine($"Dien tich hinh tam giac = {S}");
+        }
+        else
+        {
+            Console.WriteLine("Thong tin khong hop le !!!");
+        }
+    }
+}
+public class Hinhvuong: Hinhhoc
+{
+    private double canh;
+    public void Nhap()
+    {
+        Console.WriteLine("---Hinh Vuong--- ");
+        Console.Write("Nhap canh cua hinh vuong: ");
+        canh=double.Parse(Console.ReadLine());
+    }
+    public override void Chuvi()
+    {
+        Console.WriteLine($"Chu vi hinh vuong = {canh*4} ");
+    }
+    public override void Dientich()
+    {
+        Console.WriteLine($"Dien tich hinh vuong = {canh * canh}");
+    }
+}
+class Program
+{
+    static void Main ()
+    {
+        Hinhchunhat a = new Hinhchunhat();
+        a.Nhap();
+        a.Chuvi();
+        a.Dientich();
+        Hinhtron b = new Hinhtron();
+        b.Nhap();
+        b.Chuvi();
+        b.Dientich();
+        Hinhtamgiac c = new Hinhtamgiac();
+        c.Nhap();
+        c.Chuvi();
+        c.Dientich();
+        Hinhvuong d = new Hinhvuong();
+        d.Nhap();
+        d.Chuvi();
+        d.Dientich();
+    }
+}
---
+++ C6/BTSlide2/Program.cs
@@ -0,0 +1,40 @@
+﻿interface tinhnang
+{
+    void kiemtra(int i);
+}
+class Quat : tinhnang
+{
+    public void kiemtra(int i)
+    {
+        if (i==0) Console.WriteLine("Quat dang bat");
+        else Console.WriteLine("Quat dang tat");
+    }
+}
+class DieuHoa : tinhnang
+{
+    public void kiemtra(int i)
+    {
+        if (i==0) Console.WriteLine("Dieu Hoa dang bat");
+        else Console.WriteLine("Dieu Hoa dang tat");
+    }
+}
+class Tivi : tinhnang
+{
+    public void kiemtra(int i)
+    {
+        if (i==0) Console.WriteLine("Tivi dang bat");
+        else Console.WriteLine("Tivi dang tat");
+    }
+}
+class program
+{
+    static void Main()
+    {
+        Quat a = new Quat();
+        a.kiemtra(0);
+        DieuHoa b = new DieuHoa();
+        b.kiemtra(1);
+        Tivi c = new Tivi();
+        c.kiemtra(2);
+    }
+}
